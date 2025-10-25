52469914281461398c51fe5a8e272b0c27caaaa9
aeaeo <msvar655@gmail.com>
2025-10-25 20:14:21 +0300

    bjson decoder init

---
+++ src/bej.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "common.h"
+
+/*
+* converts BEJ encoded data to JSON format.
+*/
+bool bej_to_json(const uint8_t *bej_data, size_t bej_size,
+                 const uint8_t *schema_dict_data, size_t schema_dict_size,
+                 const uint8_t *anno_dict_data, size_t anno_dict_size,
+                 char *output_buffer, size_t output_buffer_size,
+                 size_t *output_length);
+

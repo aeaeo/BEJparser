9c2b91b107771f6c47b6a9c94f83c18ff7a9c891
aeaeo <msvar655@gmail.com>
2025-10-26 15:33:10 +0300

    main.c refactor

--- src/main.c
+++ src/main.c
@@ -1,6 +1,57 @@
 #include <getopt.h>
 #include "bej.h"
 
+/*
+ * Actually just prints help information.
+ */
+static void
+print_usage(const char *program_name)
+{
+	fprintf(stderr,
+		"Overview: binary encoded json decoder to json format\n\n"
+		"Usage: %s [-a <annotation_dictionary_file>] -s <schema_dictionary_file> -b <bej_file> [-o <output_file>]\n\n"
+		"Options:\n\n"
+			"\t-a\tSpecify the annotation dictionary file (optional)\n"
+			"\t-b\tSpecify the BEJ binary file to decode (required)\n"
+			"\t-h\tShow this message\n"
+			"\t-o\tSpecify output JSON file (default: stdout)\n"
+			"\t-s\tSpecify the schema dictionary file (required)\n",
+		program_name);
+}
+
+/*
+ * General function to read the entire content of a file into a buffer.
+ */
+static size_t
+read_file(const char *filename, uint8_t *buffer, size_t max_size)
+{
+    FILE *f = fopen(filename, "rb");
+    if (!f) {
+        fprintf(stderr, "Error: Failed to open file %s\n", filename);
+        return 0;
+    }
+    
+    fseek(f, 0, SEEK_END);
+    size_t file_size = (size_t)ftell(f);
+    fseek(f, 0, SEEK_SET);
+    
+    if (file_size < 0 || file_size > max_size) {
+        fprintf(stderr, "Error: File %s is too large or invalid\n", filename);
+        fclose(f);
+        return 0;
+    }
+    
+    size_t bytes_read = fread(buffer, 1, file_size, f);
+    fclose(f);
+    
+    if (bytes_read != file_size) {
+        fprintf(stderr, "Error: Failed to read complete file '%s'\n", filename);
+        return 0;
+    }
+    
+    return bytes_read;
+}
+
 int
 main(int argc, char** argv)
 {
@@ -10,68 +61,39 @@ main(int argc, char** argv)
 	size_t anno_dict_size = 0UL;
 	uint8_t bej_data[65536] = {0};
 	size_t bej_size = 0UL;
-	FILE *f = NULL;
 
-	const char* desc =
-		"Overview: binary encoded json decoder to json format\n\n"
-		"Usage: <program_name> [-a <annotation_dictionary_file>] [-s <schema_dictionary_file>] [-b <bjson_file>]\n\n"
-		"Options:\n\n"
-			"\t-a\tSpecify the annotation dictionary file\n"
-			"\t-b\tSpecify the BEJ binary file to decode\n"
-			"\t-h\tShow this message\n"
-			"\t-s\tSpecify the schema dictionary file\n";
+	char* anno_file = NULL;
+	char* schema_file = NULL;	// TODO: make optional; also possibly no need of this due to simplification
 
-	int option = getopt(argc, argv, "ha:b:s:");
+	char* bej_file = NULL;
+	char* output_file = NULL;
+
+	int option = getopt(argc, argv, "ha:b:s:o:");
+
 	while (option != EOF) {
 		switch (option) {
 		case 'a':
-			f = fopen(optarg, "rb");
-			if (!f) {
-				fprintf(stderr, "Failed to open annotation dictionary file\n");
-				return -1;
-			}
-			fseek(f, 0, SEEK_END);
-			anno_dict_size = ftell(f);
-			fseek(f, 0, SEEK_SET);
-			fread(anno_dict_data, 1, anno_dict_size, f);
-			fclose(f);
-			f = NULL;
+			anno_file = optarg;
 			break;
 		case 'b':
-			f = fopen(optarg, "rb");
-			if (!f) {
-				fprintf(stderr, "Failed to open binary json file\n");
-				return -1;
-			}
-			fseek(f, 0, SEEK_END);
-			bej_size = ftell(f);
-			fseek(f, 0, SEEK_SET);
-			fread(bej_data, 1, bej_size, f);
-			fclose(f);
-			f = NULL;
+			bej_file = optarg;
 			break;
 		case 's':
-			f = fopen(optarg, "rb");
-			if (!f) {
-				fprintf(stderr, "Failed to open schema dictionary file\n");
-				return -1;
-			}
-			fseek(f, 0, SEEK_END);
-			schema_dict_size = ftell(f);
-			fseek(f, 0, SEEK_SET);
-			fread(schema_dict_data, 1, schema_dict_size, f);
-			fclose(f);
-			f = NULL;
+			schema_file = optarg;
+			break;
+		case 'o':
+			output_file = optarg;
 			break;
 		case 'h':
 		default:
-			fprintf(stderr, "%s", desc);
+			print_usage(argv[0]);
 			return -1;
 		}
 	}
 
-	if (argc <= 3 || argc > 4 || bej_size == 0UL || schema_dict_size == 0UL || anno_dict_size == 0UL) {
-		fprintf(stderr, desc);
+	if (!bej_file || !schema_file) {
+		fprintf(stderr, "Error: missed required arguments\n\n");
+		print_usage(argv[0]);
 		return -1;
 	}
 

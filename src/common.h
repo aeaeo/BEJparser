e9d6027e16d61a70d57bcad77707a776066f5c88
aeaeo <msvar655@gmail.com>
2025-10-28 21:54:58 +0300

    added some print&read defines

--- src/common.h
+++ src/common.h
@@ -1,6 +1,28 @@
 #pragma once
+#include <stdbool.h>
 #include <stddef.h> // size_t
 #include <stdint.h>
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdlib.h>
 #include <string.h>
+
+#define READ_U8_AND_INC(ptr, off) ((uint8_t)(ptr[off++]))
+#define READ_U16_LE(ptr, off) ((uint16_t)(ptr[off + 1] << 8) | (uint16_t)(ptr[off]))
+#define READ_U32_LE(ptr, off) ((uint32_t)(ptr[off + 3] << 24) | ((uint32_t)(ptr[off + 2] << 16)) | \
+                              ((uint32_t)(ptr[off + 1] << 8)) | (uint32_t)(ptr[off]))
+
+#define errmsg(fmt, ...) \
+    fprintf(stderr, "Error at %s():%d What: " fmt "\n", __func__, __LINE__, ##__VA_ARGS__)
+#define warnmsg(fmt, ...) \
+    fprintf(stdout, "Warning at %s():%d What: " fmt "\n", __func__, __LINE__, ##__VA_ARGS__)
+#ifdef NDEBUG
+#define dbgmsg(fmt, ...) \
+    fprintf(stdout, "[DEBUG] %s():%d: " fmt "\n", __func__, __LINE__, ##__VA_ARGS__)
+#else
+#define dbgmsg(fmt, ...) ((void)0)
+#endif /* NDEBUG */
+#define infomsg(fmt, ...) \
+    fprintf(stdout, fmt "\n", ##__VA_ARGS__)
+
+#define SUCCESS 0
+#define FAILURE 1
